8169027d9d745d4a77998820736e507700db7880
Wallace Verje <wallacev2@gmail.com>
2012-05-25 23:53:26 -0300

    Menu drop  down update2

--- recursos/javascript/efeitoBarra.js
+++ recursos/javascript/efeitoBarra.js
@@ -2,20 +2,34 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
+var colorMouseEnterFonteContainer = "#bbc1cb"
+var colorMouseOutFonteContainer = "#FFF"
+var colorBackgroundEnterContainer = "#c43f3f"
+var colorBackgroundOutContainer = "#2652d6"
+var bordaOn = "1px solid #fff"
+var bordaOff = ""
 
 $(document).ready(function(){
     $("#barraLinkSuperior a").mouseenter(function(){
         $(this).stop().animate({
-            "background-color": "#f3f5fc",
-            "color": "#2652d6"
+            "background-color": colorBackgroundEnterContainer,
+            "color": colorMouseEnterFonteContainer,
+//            "border-top": bordaOn,
+            "border-bottom": bordaOn
         },600)
+        
+//        $("#nav ul a").css({
+//            "border-top": bordaOff,
+//            "border-bottom": bordaOff
+//        })
     })
     
     $("#barraLinkSuperior a").mouseout(function(){
         $(this).stop().animate({
-            "background-color":"#2652d6",
-            "color":"#FFF"
+            "background-color": colorBackgroundOutContainer,
+            "color":colorMouseOutFonteContainer,
+//            "border-top": bordaOff,
+            "border-bottom": bordaOff
         },200)
     })
     
@@ -24,15 +38,28 @@ $(document).ready(function(){
     
     $("#barraLinkInferior a").mouseenter(function(){
         $(this).stop().animate({
-            "background-color": "#f3f5fc",
-            "color": "#2652d6"
+            "background-color": colorBackgroundEnterContainer,
+            "color": colorMouseEnterFonteContainer
         },600)
     })
     
     $("#barraLinkInferior a").mouseout(function(){
         $(this).stop().animate({
-            "background-color":"#2652d6",
-            "color":"#FFF"
+            "background-color": colorBackgroundOutContainer,
+            "color": colorMouseOutFonteContainer
         },200)
     })
 })
+
+
+//$(function(){
+//    
+//    $("#barraLinkSuperior a").mousedown(function(){
+//        var elemento  = document.getElementById(this).click();
+//        elemento.className = 'selected'
+//    })
+//
+//})
+
+    
+    
--- recursos/javascript/subMenu.js
+++ recursos/javascript/subMenu.js
@@ -3,41 +3,21 @@
  * and open the template in the editor.
  */
 
-$(function(){
-    $("#navegaSuperior a").mouseover(function(){
-        var menu = $(this).parent().children('.subMenu');
-        var submenu =  $(this).parent().parent();
-          
-        if(menu.length > 0 && menu.is(':hidden')){
-            $("#navegaSuperior a").removeClass('ativo');
-            $(this).addClass('ativo');
-            $('.subMenu').slideUp();    
-            menu.slideDown();
-        }
-        if(!submenu.hasClass('subMenu') && menu.length == 0){
-            $("#navegaSuperior a").removeClass('ativo');
-            $(this).addClass('ativo');
+$(document).ready(function () {	
+	
+    $('#nav li').hover(
+        function () {
+            //show its submenu
+            $('ul', this).slideDown(500);
 
-            $('.subMenu').slideUp();
+        }, 
+        function () {
+            //hide its submenu
+            $('ul', this).slideUp(400);			
         }
-          
-    });
-       
-    $("#titulo").mouseover(function(){
-        $(".subMenu").slideUp();        
-    }); 
-    $("#bannerSuperior").mouseover(function(){
-        $(".subMenu").slideUp();        
-    }); 
-    $("#barraLinkInferior").mouseover(function(){
-        $(".subMenu").slideUp();        
-    }); 
-    
-    $("#conteudo").mouseover(function(){
-        $(".subMenu").slideUp();        
-    }); 
+        );
+	
 });
-       
        
        
        
