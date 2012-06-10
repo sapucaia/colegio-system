229185c87c9d21e7004f757af12bc6e8110bcabf
Paavo Soeiro <paavomsoeiro@gmail.com>
2012-06-10 19:01:03 -0300

    pagina principal admin

--- app/index.php
+++ app/index.php
@@ -1,12 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link type="text/css" rel="stylesheet" href=""/>
+        <script type="text/javascript" src="../recursos/javascript/jquery-1.4.3.min.js"></script>
+        <script type="text/javascript" src="../recursos/javascript/carregamentoPaginaAdm.js"></script>
+        <title></title>
+    </head>
+    <body>
+        <h1>M&oacute;dulo de administra&ccedil;&atilde;o</h1>
 
-require_once('../core/dispatcher/Command.php');
-require_once('../core/dispatcher/UrlInterpreter.php');
-require_once('../core/dispatcher/CommandDispatcher.php');
-require_once('../core/dispatcher/Controller.php');
+        <div id="menuPrincipal">
+            <ul>
+                <li><a href="app/views/aviso/index.php">Avisos</a></li>
+                <li><a href="">Recados</a></li>
+                <li><a href="adm/contatoAdm.php">Contato</a></li>
+                <li><a href="adm/videoAdm.php">V&iacute;deos</a></li>
+            </ul>
 
-$urlInterpreter = new UrlInterpreter();
-$command = $urlInterpreter->getCommand();
-$commandDispatcher = new CommandDispatcher($command, true);
-$commandDispatcher->Dispatch();
-?>
+        </div>
+        <?php
+        require_once('../core/dispatcher/Command.php');
+        require_once('../core/dispatcher/UrlInterpreter.php');
+        require_once('../core/dispatcher/CommandDispatcher.php');
+        require_once('../core/dispatcher/Controller.php');
+
+        $urlInterpreter = new UrlInterpreter();
+        $command = $urlInterpreter->getCommand();
+        $commandDispatcher = new CommandDispatcher($command, true);
+        $commandDispatcher->Dispatch();
+        ?>
+    </body>
+</html>
