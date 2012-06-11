418e56e7ca189723add8bedd23b4d305ca2ff1c7
Paavo Soeiro <paavomsoeiro@gmail.com>
2012-06-11 01:57:32 -0300

    noticias

---
+++ app/models/Noticia.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Description of Noticia
+ *
+ * @author Paavo Soeiro
+ */
+class Noticia {
+    //put your code here
+}
+
+?>
---
+++ app/models/NoticiaRecord.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Description of NoticiaRecord
+ *
+ * @author Paavo Soeiro
+ */
+class NoticiaRecord {
+    //put your code here
+}
+
+?>
