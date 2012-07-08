6b59a70d5f6809fc19842dc620bbf7647b9d2683
Paavo Soeiro <paavomsoeiro@gmail.com>
2012-07-08 15:26:28 -0300

    recadoAdm e avisoAdm

---
+++ avisoAdm.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link type="text/css" rel="stylesheet" href="recursos/css/formulario.css"/>
+        <script type="text/javascript" src="recursos/javascript/jquery-1.4.3.min.js"></script>
+        <script type="text/javascript" src="recursos/javascript/carregamentoPaginaAdm.js"></script>
+        <title></title>
+    </head>
+    <body>
+        <h1>Administração - Avisos </h1>
+        <div id="botoes">
+            <ul id="btAdministra">
+                <li><a href="app/views/aviso/novo.php">Adcionar avisos</a></li>
+                <li><a href="#">Remover avisos</a></li>
+                <li><a href="app/views/aviso/editar.php">Editar</a></li>
+                <li><a href="app/views/aviso/mostrar.php">Mostrar avisos</a></li>
+            </ul>
+        </div>
+        <p><a href="admMenuPrincipal.html">Voltar</a></p>
+        <div id="corpo">
+            
+        </div>
+        
+    </body>
+</html>
